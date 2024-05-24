50754cf694d212c9184e427d9b15bd96f484e50c
Utkarsh <k20pro@duck.com>
2024-05-24 07:19:49 +0530

    updated menu ka navbar

--- src/Components/Menu/Navbar.jsx
+++ src/Components/Menu/Navbar.jsx
@@ -1,19 +1,18 @@
-import React, { Component } from "react";
+import React, { useState } from 'react';
 import Gallery from "./Gallery";
-import "./NavbarStyles.css";
-import { NavLink } from "react-router-dom";
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  
-  class Navbar extends Component {
-    state = { clicked: false };
-   
-  
-    
-  handleClick = () => {
-    this.setState({ clicked: !this.state.clicked });
+
+const Navbar = () => {
+  const [isNavVisible, setIsNavVisible] = useState(false);
+
+  const handleOpenNav = () => {
+    setIsNavVisible(true);
   };
 
-  scrollToCategory = (category) => {
+  const handleCloseNav = () => {
+    setIsNavVisible(false);
+  };
+
+  const scrollToCategory = (category) => {
     const categoryElement = document.getElementById(category.replace(/\s+/g, ''));
     if (categoryElement) {
       categoryElement.scrollIntoView({ behavior: 'smooth' });
@@ -21,69 +20,134 @@ import { NavLink } from "react-router-dom";
       categoryTitle.click();
     }
   };
-    render(){
-    return(
-        <>
-            <nav>
-            
-               <a href="http://localhost:3000/">
-                <svg id="logo-15" width="49" height="48" viewBox="0 0 49 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                     <path d="M24.5 12.75C24.5 18.9632 19.4632 24 13.25 24H2V12.75C2 6.53679 7.03679 1.5 13.25 1.5C19.4632 1.5 24.5 6.53679 24.5 12.75Z" class="ccustom" fill="#17CF97"></path>
-                     <path d="M24.5 35.25C24.5 29.0368 29.5368 24 35.75 24H47V35.25C47 41.4632 41.9632 46.5 35.75 46.5C29.5368 46.5 24.5 41.4632 24.5 35.25Z" class="ccustom" fill="#17CF97"></path>
-                     <path d="M2 35.25C2 41.4632 7.03679 46.5 13.25 46.5H24.5V35.25C24.5 29.0368 19.4632 24 13.25 24C7.03679 24 2 29.0368 2 35.25Z" class="ccustom" fill="#17CF97"></path>
-                     <path d="M47 12.75C47 6.53679 41.9632 1.5 35.75 1.5H24.5V12.75C24.5 18.9632 29.5368 24 35.75 24C41.9632 24 47 18.9632 47 12.75Z" class="ccustom" fill="#17CF97"></path>
-                </svg>
-               </a>
 
-               <div>
-               <ul id="navbar" className={this.state.clicked ? "navbar active" : "navbar"} >
-              <li>
-              <NavLink className='active' to="/">Home</NavLink>
-              </li>
-              <li>
-                <a href="#Indian" onClick={() => this.scrollToCategory('Indian')}>
+  const navStyles = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '250px',
+    height: '100vh',
+    transform: isNavVisible ? 'translateX(0)' : 'translateX(-250px)',
+    transition: 'transform 0.3s ease-in-out',
+    zIndex: 1000,
+  };
+
+  const navBlackStyles = {
+    ...navStyles,
+    backgroundColor: 'rgb(60,14,175)',
+ 
+    maxWidth: '480px',
+    minWidth: '320px',
+    transitionDelay: isNavVisible ? '0s' : '0.4s',
+  };
+
+  const navRedStyles = {
+    ...navStyles,
+    width: '95%',
+    backgroundColor: 'rgb(229, 9, 20)',
+    transitionDelay: isNavVisible ? '0.2s' : '0s',
+  };
+
+  const navWhiteStyles = {
+    ...navStyles,
+    backgroundColor:'rgb(34, 31, 31)',
+    width: '95%',
+    padding: '40px',
+    position: 'relative',
+    transitionDelay: isNavVisible ? '0.4s' : '0s',
+  };
+
+  const closeButtonStyles = {
+    opacity: 0.3,
+    position: 'absolute',
+    top: '20px',
+    right: '20px',
+  };
+
+  const listItemStyles = {
+    margin: '20px 0',
+  };
+
+  const linkStyles = {
+    color: 'rgb(256,256,256)',
+    fontSize: '21px',
+    
+    textDecoration: 'none',
+    textTransform: 'uppercase',
+  };
+
+  const subListStyles = {
+    listStyleType: 'none',
+    paddingLeft: '20px',
+  };
+
+  const openBtnStyles = {
+    backgroundColor: 'transparent',
+    border: 'none',
+    color: '#fff',
+    fontSize: '24px',
+    position: 'fixed',
+    top: '20px',
+    left: '20px',
+    cursor: 'pointer',
+    zIndex: 1001,
+    display: isNavVisible ? 'none' : 'block', // Add this line
+  };
+
+  return (
+    <div>
+      {!isNavVisible && ( // Add this condition
+        <button className="nav-btn open-btn" onClick={handleOpenNav} style={openBtnStyles}>
+          <i className="fas fa-bars"></i>
+        </button>
+      )}
+
+      <div style={navBlackStyles}>
+        <div style={navRedStyles}>
+          <div style={navWhiteStyles}>
+            <button className="nav-btn close-btn" onClick={handleCloseNav} style={closeButtonStyles}>
+              <i className="fas fa-times"></i>
+            </button>
+            
+
+            <ul className="list" style={{ listStyleType: 'none', padding: 0 , paddingTop:'50px' }}>
+              <li style={listItemStyles}>
+                <a style={linkStyles} href="#Indian" onClick={() => scrollToCategory('Indian')}>
                   Indian
                 </a>
               </li>
-              <li>
-                <a href="#Continental" onClick={() => this.scrollToCategory('Continental')}>
+              <li style={listItemStyles}>
+                <a style={linkStyles} href="#Continental" onClick={() => scrollToCategory('Continental')}>
                   Continental
                 </a>
               </li>
-              <li>
-                <a href="#Asian" onClick={() => this.scrollToCategory('Asian')}>
+              <li style={listItemStyles}>
+                <a style={linkStyles} href="#Asian" onClick={() => scrollToCategory('Asian')}>
                   Asian
                 </a>
               </li>
-              <li>
-                <a href="#Beverages" onClick={() => this.scrollToCategory('Beverages')}>
+              <li style={listItemStyles}>
+                <a style={linkStyles} href="#Beverages" onClick={() => scrollToCategory('Beverages')}>
                   Beverages
                 </a>
               </li>
-              <li>
-                <a href="#Deserts" onClick={() => this.scrollToCategory('Deserts')}>
+              <li style={listItemStyles}>
+                <a style={linkStyles} href="#Deserts" onClick={() => scrollToCategory('Deserts')}>
                   Deserts
                 </a>
               </li>
-              <li>
-                <a href="#Starters" onClick={() => this.scrollToCategory('Starters')}>
+              <li style={listItemStyles}>
+                <a style={linkStyles} href="#Starters" onClick={() => scrollToCategory('Starters')}>
                   Starters
                 </a>
               </li>
             </ul>
           </div>
-
-          <div id="mobile" onClick={this.handleClick}>
-            <i
-              id="bar"
-              className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
-            ></i>
-          </div>
-        </nav>
-        <Gallery />
-      </>
-    );
-  }
-}
+        </div>
+      </div>
+      <Gallery />
+    </div>
+  );
+};
 
 export default Navbar;
